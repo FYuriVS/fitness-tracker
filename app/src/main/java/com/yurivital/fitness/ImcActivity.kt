aaa5d04da38ba62154c6c7880d12fae67d706526
yurivital <yvital@guarany.com.br>
2024-04-26 17:32:57 -0300

    segundo commit

--- app/src/main/java/com/yurivital/fitness/ImcActivity.kt
+++ app/src/main/java/com/yurivital/fitness/ImcActivity.kt
@@ -1,15 +1,17 @@
 package com.yurivital.fitness
 
+import android.content.Context
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.getSystemService
 import com.yurivital.fitness.databinding.ActivityImcBinding
-
 
 class ImcActivity : AppCompatActivity() {
 
@@ -27,6 +29,8 @@ class ImcActivity : AppCompatActivity() {
         editHeight = binding.editImcHeight
         btnSend = binding.btnImcSend
 
+
+// Colocar evento de click
         btnSend.setOnClickListener {
             if (!validate()) {
                 Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
@@ -39,6 +43,8 @@ class ImcActivity : AppCompatActivity() {
 
             val imcResponseId = imcResponse(result)
 
+
+// Chamar Dialog;
             AlertDialog.Builder(this)
                 .setTitle(getString(R.string.imc_response, result))
                 .setMessage(imcResponseId)
@@ -47,6 +53,11 @@ class ImcActivity : AppCompatActivity() {
                 }
                 .create()
                 .show()
+
+
+// Capturar e manipular serviço de sistema: No caso foi de teclado;
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
     }
 
